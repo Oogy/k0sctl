fc21bc20ec428222c3a934fc2c3b7cd41a471331
jasmingacic <jasmin.gacic@gmail.com>
2021-06-04 10:52:55 +0200

    Terraform AWS HA example for k0s
    Signed-off-by: jasmingacic <jasmin.gacic@gmail.com>

---
+++ examples/aws-tf-ha/variables.tf
@@ -0,0 +1,10 @@
+
+variable "controller_count" {
+  type    = number
+  default = 3
+}
+
+variable "worker_count" {
+  type    = number
+  default = 1
+}
---
+++ examples/aws-tf/output.tf
@@ -0,0 +1,15 @@
+// aws_instance" "cluster-controller
+// aws_instance" "cluster-workers
+output "controller-instances"{
+  description = "List of IDs of controller instances"
+  value       = aws_instance.cluster-controller
+}
+
+output "worker-instances"{
+  description = "List of IDs of worker instances"
+  value       = aws_instance.cluster-workers
+}
+
+output "cluster-security_group"{
+    value = aws_security_group.cluster_allow_ssh.id
+}
